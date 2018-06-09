cbedfcc36561eca81f9843d417fbcdd5f8052a84
Elka165 <ela_dolna99@o2.pl>
2018-06-09 18:56:51 +0200

    try_catch

--- Main.java
+++ Main.java
@@ -1,46 +1,40 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
-        Scanner scaner = new Scanner(System.in);
-        Card card2 = new Card();
 
-        try {
-            System.out.println("Rodzaje figur: ");
-            Ranks[] var3 = Ranks.values();
-            int var4 = var3.length;
+//bez skanera
 
-            int var5;
-            for(var5 = 0; var5 < var4; ++var5) {
-                Ranks algo = var3[var5];
-                System.out.println(algo);
-            }
+  //  Card card=new Card( Ranks.ACE, Suits.CLUBS);
+   // System.out.println(card.getDescription());
 
-            System.out.println("Wybierz figurę: ");
-            card2.setRanks(Ranks.valueOf(scaner.next()));
-            System.out.println("Rodzaje kolorów: ");
-            Suits[] var8 = Suits.values();
-            var4 = var8.length;
+//ze skanerem z gwiazdką
 
-            for(var5 = 0; var5 < var4; ++var5) {
-                Suits algo = var8[var5];
-                System.out.println(algo);
-            }
+        Scanner scaner=new Scanner(System.in);
+        Card card2=new Card();
+        try{
+        System.out.println("Rodzaje figur: " );
+        for (Ranks algo : Ranks.values()) {
+            System.out.println(algo);
+        }
+        System.out.println("Wybierz figurę: ");
 
-            System.out.println("Wybierz kolor: ");
-            card2.setSuits(Suits.valueOf(scaner.next()));
-            System.out.println(card2.getDescription());
-        } catch (IllegalArgumentException var7) {
-            System.out.println("Jedno z wyrażeń jest spoza listy, spróbuj ponownie");
+        card2.setRanks(Ranks.valueOf(scaner.next()));
+
+        System.out.println("Rodzaje kolorów: " );
+        for (Suits algo : Suits.values()) {
+            System.out.println(algo);
         }
 
+        System.out.println("Wybierz kolor: ");
+        card2.setSuits(Suits.valueOf(scaner.next()));
+
+
+
+            System.out.println(card2.getDescription());
+        }catch(IllegalArgumentException e){
+            System.out.println("Jedno z wyrażeń jest spoza listy, spróbuj ponownie");
+        }
     }
 }
